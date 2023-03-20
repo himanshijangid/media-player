3e557e5c2c60f23207ae7752ffefb72af51e1099
Himanshi Jangid <himanshijangid444@gmail.com>
2023-03-20 16:37:38 +0530

    cd

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-var index = 0
+const img = document.querySelector("img");
+const audio = document.querySelector("audio");
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
 
-function changeColors() {
-    
-    var colors =['red','gree','blue','yellow','pink','orange','seagreen','skyblue','gray','black']
+const songs = [{
+    audio: "audio/audio01.mp3",
+    img : "img/image01.jpg"
+},
+{
+    audio: "audio/audio02.mp3",
+    img : "img/image02.jpg"
+},
+{
+    audio: "audio/audio03.mp3",
+    img : "img/image03.jpg"
+}]
 
-    document.getElementsByTagName("body")[0].style.background=colors [index++];
-
-if(index > colors.length-1){
-    index=0;
+const loadSong = (songs) => {
+    audio.textContent = songs.audio;
+    img.textContent = songs.img;
 }
 
-}
+loadSong(songs[1]);
+
+
+
+let isPlaying = false;
+
+const playAudio = () => {
+    isPlaying = true;
+    audio.play();
+    play.className.replace("fa-play", "fa-pause");
+};
+
+const pauseAudio = () => {
+    isPlaying = false;
+    audio.pause();
+    play.className.replace("fa-pause", "fa-play");
+};
+
+play.addEventListener("click",() => {
+    if(isPlaying){
+        pauseAudio();
+
+    }else{
+        playAudio();
+    }
+})
+
+
+
+
+
+
+
